857fed72f9f393cd842369e2ac383ee8cf99c44a
Caleb Spare <cespare@gmail.com>
2022-04-03 13:39:50 -0700

    Add another test

--- assetserver_test.go
+++ assetserver_test.go
@@ -5,10 +5,15 @@ import (
 	"embed"
 	"fmt"
 	"io/fs"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
+	"testing/fstest"
 
 	"github.com/cespare/webtest"
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestTag(t *testing.T) {
@@ -17,9 +22,9 @@ func TestTag(t *testing.T) {
 		name string
 		want string
 	}{
-		{"d/style.css", "d/style." + tag("style\n") + ".css"},
-		{"a.js", "a." + tag("ajs\n") + ".js"},
-		{"d/sub/noext", "d/sub/noext." + tag("<!doctype html>\n")},
+		{"d/style.css", "d/style." + hashTag("style\n") + ".css"},
+		{"a.js", "a." + hashTag("ajs\n") + ".js"},
+		{"d/sub/noext", "d/sub/noext." + hashTag("<!doctype html>\n")},
 	} {
 		got, err := s.Tag(tt.name)
 		if err != nil {
@@ -53,7 +58,7 @@ func TestRemoveTag(t *testing.T) {
 	}
 }
 
-func tag(text string) string {
+func hashTag(text string) string {
 	b := sha256.Sum256([]byte(text))
 	return makeTag(b[:])
 }
@@ -78,7 +83,53 @@ func TestServeHTTPEmbed(t *testing.T) {
 	webtest.TestHandler(t, "testdata/servehttp.txt", s)
 }
 
-// TODO: add test for large file contents (>512 bytes).
+// Exercise content sniffing and hashing on files larger than 512 bytes.
+func TestServeLargeFiles(t *testing.T) {
+	content := make([]byte, 100e3)
+	for i := range content {
+		// Include 0 bytes to be detected as a binary file type.
+		content[i] = byte(i)
+	}
+	tag := hashTag(string(content))
+	fsys := fstest.MapFS{
+		"d/f": &fstest.MapFile{Data: content},
+	}
+	s := New(fsys)
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/d/f", nil)
+	s.ServeHTTP(w, req)
+	resp := w.Result()
+	checkResponseCode(t, resp, 200)
+	checkResponseBody(t, resp, content)
+	checkResponseHeader(t, resp, "ETag", `"`+tag+`"`)
+}
+
+func checkResponseCode(t *testing.T, resp *http.Response, want int) {
+	t.Helper()
+	if resp.StatusCode != want {
+		t.Fatalf("got response status %d; want %d", resp.StatusCode, want)
+	}
+}
+
+func checkResponseBody(t *testing.T, resp *http.Response, want []byte) {
+	t.Helper()
+	got, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// Shouldn't happen due to ResponseRecorder guarantees.
+		t.Fatalf("error reading response body: %s", err)
+	}
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Fatalf("wrong response body (-got, +want):\n%s", diff)
+	}
+}
+
+func checkResponseHeader(t *testing.T, resp *http.Response, header, want string) {
+	t.Helper()
+	got := resp.Header.Get(header)
+	if got != want {
+		t.Fatalf("wrong value for header %q: want %q; got %q", header, got, want)
+	}
+}
 
 func TestPrintHashes(t *testing.T) {
 	t.Skip("un-skip this test to print out all testdata asset hashes")
@@ -94,7 +145,7 @@ func TestPrintHashes(t *testing.T) {
 		if err != nil {
 			return err
 		}
-		fmt.Printf("%s\t%s\n", p, tag(string(b)))
+		fmt.Printf("%s\t%s\n", p, hashTag(string(b)))
 		return nil
 	})
 	if err != nil {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/cespare/assetserver
 
 go 1.17
 
-require github.com/cespare/webtest v0.2.0
+require (
+	github.com/cespare/webtest v0.2.0
+	github.com/google/go-cmp v0.5.7
+)
